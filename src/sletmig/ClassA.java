6afeb59512b26948f9849755d31410969ef64a5f
lam <lam@cph-cnu1470h3l.efif.dk>
2013-01-30 14:18:31 +0100

    Initial Commit including two empty files

---
+++ src/sletmig/ClassA.java
@@ -0,0 +1,7 @@
+package sletmig;
+/**
+ * TODO
+ */
+public class ClassA {
+
+}
---
+++ src/sletmig/SletMig.java
@@ -0,0 +1,14 @@
+
+package sletmig;
+
+
+public class SletMig {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
